a8beeab2a416f87b5323de26a924ce7f0e316873
Alain Galvan <hi@alain.xyz>
2017-04-26 21:55:10 -0400

    Changed main.rs

--- codevr/src/main.rs
+++ codevr/src/main.rs
@@ -14,16 +14,23 @@ use std::net;
 
 fn main() {
     // Initialize application state
-    // @TODO - read from config files
+    // @TODO - read_config();
     let state = 0;
 
     // Import and create descriptor tree of application
+    // @TODO - App::new();
     let app = 0;
 
     // Start Coronal engine
-    // @TODO - Pass state as props,
+    // @TODO - Engine::new(state, app);
     let engine = 0;
 
     //while engine.update()
-    {}
+    {
+        //engine.io();  
+        //engine.begin_update();      
+        //engine.update();
+        //engine.render();
+        //engine.end_update();
+    }
 }
