e269828844dfe432eca5a3a65a88e29d2371ea56
Alain Galvan <hi@alain.xyz>
2017-05-01 21:42:55 -0400

    Hooking up Vulkan renderer to engine

--- codevr/src/engine/mod.rs
+++ codevr/src/engine/mod.rs
@@ -1,12 +1,21 @@
 use winit::{WindowBuilder, get_available_monitors, get_primary_monitor};
 use vulkano_win::{Window, VkSurfaceBuild, required_extensions};
-use vulkano::instance::Instance;
+use vulkano::instance::{Instance, PhysicalDevice};
+use vulkano::device::{Queue, Device, DeviceExtensions};
+use vulkano::swapchain::{Swapchain, SurfaceTransform};
 
 use config::Config;
 use config::WindowConfig;
 
+use std::sync::Arc;
+
 pub struct Engine {
     window: Window,
+    instance: Arc<Instance>,
+    device: Arc<Device>,
+    swapchain: Arc<Swapchain>,
+    queue: Arc<Queue>
+
 }
 
 impl Engine {
@@ -21,9 +30,73 @@ impl Engine {
             Instance::new(None, &extensions, None).expect("Failed to create Vulkan instance.")
         };
 
+        let physical_device = PhysicalDevice::enumerate(&instance)
+            .next()
+            .expect("No vulkan device is available.");
+
         let window = window_builder.build_vk_surface(&instance).unwrap();
 
-        Engine { window }
+        let queue = physical_device
+            .queue_families()
+            .find(|q| q.supports_graphics() && window.surface().is_supported(q).unwrap_or(false))
+            .expect("Couldn't find a graphical queue family.");
+
+        // Logical Device, Queues
+        let (device, mut queues) = {
+            let device_ext = DeviceExtensions {
+                khr_swapchain: true,
+                ..DeviceExtensions::none()
+            };
+
+            Device::new(&physical_device,
+                        physical_device.supported_features(),
+                        &device_ext,
+                        [(queue, 0.5)].iter().cloned())
+                    .expect("failed to create device")
+        };
+
+        // Device Queue
+        let queue = queues.next().unwrap();
+
+        // Swapchain, Swapchain Images
+        let (swapchain, images) = {
+
+            let caps = window
+                .surface()
+                .get_capabilities(&physical_device)
+                .expect("failed to get surface capabilities");
+
+            let dimensions = caps.current_extent.unwrap_or([1280, 720]);
+
+            let present = caps.present_modes.iter().next().unwrap();
+
+            let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+
+            let format = caps.supported_formats[0].0;
+
+            Swapchain::new(&device,
+                           &window.surface(),
+                           caps.min_image_count,
+                           format,
+                           dimensions,
+                           1,
+                           &caps.supported_usage_flags,
+                           &queue,
+                           SurfaceTransform::Identity,
+                           alpha,
+                           present,
+                           true,
+                           None)
+                    .expect("failed to create swapchain")
+        };
+
+        Engine {
+            window: window,
+            instance: instance.clone(),
+            device,
+            swapchain,
+            queue
+        }
     }
 
     pub fn io(&self) {}
@@ -36,7 +109,7 @@ impl Engine {
 }
 
 /// Creates a window builder with a given window configuration.
-pub fn create_window(config: WindowConfig) -> WindowBuilder  {
+pub fn create_window(config: WindowConfig) -> WindowBuilder {
 
     let window_manager = WindowBuilder::new()
         .with_title("CodeVR")
@@ -54,3 +127,4 @@ pub fn create_window(config: WindowConfig) -> WindowBuilder  {
 
     window_manager.with_fullscreen(w)
 }
+
