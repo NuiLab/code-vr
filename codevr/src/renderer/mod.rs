395c0b83ab3666607d6be89b4eec9e4a63b01c46
Alain Galvan <hi@alain.xyz>
2017-05-17 12:47:34 -0400

    Started renderer module

--- codevr/src/renderer/mod.rs
+++ codevr/src/renderer/mod.rs
@@ -1,3 +1,13 @@
 struct Renderer {
-  
+    instance: Arc<Instance>,
+    physical_device: usize,
+    device: Arc<Device>,
+    swapchain: Arc<Swapchain>,
+    images: Vec<Arc<SwapchainImage>>,
+    depth_buffer: Arc<AttachmentImage<format::D16Unorm>>,
+    render_pass: Arc<render_pass::CustomRenderPass>,
+    framebuffers: Vec<Arc<Framebuffer<render_pass::CustomRenderPass>>>,
+    submissions: Vec<Arc<Submission>>,
+    queue: Arc<Queue>,
+    render_threads: Vec<JoinHandle>
 }
