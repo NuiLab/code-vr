bfd727583d31d3bad6f15f07f75f0a6cc7116ade
Lukas Borges <lborg019@fiu.edu>
2017-06-02 15:34:15 -0400

    buffered tcp receive on client side

--- languages/client/src/main.rs
+++ languages/client/src/main.rs
@@ -8,6 +8,7 @@ use encoding::all::ASCII;
 use std::thread::sleep;
 use std::io;
 use std::time::Duration;
+use std::str;
 //use std::io::prelude::*;
 
 //const HOST: &'static str = "127.0.0.1:5555";
@@ -17,19 +18,41 @@ fn check_file(command: &str, mut stream: &TcpStream) -> Result<String, Box<error
     let mut command_bytes = try!(ASCII.encode(command, EncoderTrap::Strict).map_err(|x| x.into_owned()));
     command_bytes.push('\r' as u8);            //ending escape sequence    
 
-    stream.write_all(&command_bytes).unwrap(); //write to stream (send path)
+    let _ = stream.write_all(&command_bytes).unwrap(); //send
 
-    sleep(Duration::from_millis(2));           //disgusting solution;
+    stream.set_read_timeout(None).expect("set_read_timeout call failed");
+    assert_eq!(stream.read_timeout().unwrap(), None);
+
+    //let response = String::new();
+    //let mut buf = [0; 8];
+    let mut buf: Vec<u8> = Vec::new();
+    loop {
+        let mut result = stream.read(&mut buf);
+        match result {
+            Ok(n) => {
+                println!("Received {} bytes", n);
+                buf.push(n);
+                if n < 8 { break };
+            },
+            _ => {},
+        }
+    }
+
+    //convert vec to string and print it out
+    let response = String::from_utf8(buf).unwrap();
+    println!("response: {}", response);
+
+    /*
+    //sleep(Duration::from_millis(10));          //disgusting solution;
                                                //program should wait until stream is ready
 
     let mut response = String::new();
-    let mut limited = stream.take(1024);       //read from stream (receive)
-    limited.read_to_string(&mut response);     //unwrap() causes this to panic
+    let mut buffer = [0; 1024];                //1024 bytes buffer
 
-    if response.is_empty()
-    {
-        println!("response is empty")
-    }
+    //set a timeout
+    let mut limited = stream.read(&mut [0; 1024]);       //read from stream (receive) (this blocks)
+    //limited.read_to_string(&mut response);     //unwrap() causes this to panic
+    */
 
     Ok(response)
 }
@@ -38,10 +61,10 @@ fn main() {
     //setup connection:
     //let mut stream = TcpStream::connect("127.0.0.1:5555")
     let stream = TcpStream::connect("127.0.0.1:5555") // try!(TcpStream::connect(HOST));
-                               .expect("Couldn't connect to the server...");
+                            .expect("Couldn't connect to the server...");
 
     //make stream nonblocking
-    stream.set_nonblocking(true).expect("set_nonblocking call failed");
+    //stream.set_nonblocking(true).expect("set_nonblocking call failed");
 
     loop {
         println!("Type command:");
